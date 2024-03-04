ffebbf20c67d4204074557586460dce81a6cd948
Kamenchu <iankamenchu@gmail.com>
2024-03-04 20:48:04 +0300

    completed tests

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
-
-    #Instance method definition
-    pass
+    def __init__(self, name="fido"):
+        self.name = name
+        
+        
+    def sit(self):
+        print("The dog is sitting.")
+    
+    def bark(self):  
+        print("Woof!")
